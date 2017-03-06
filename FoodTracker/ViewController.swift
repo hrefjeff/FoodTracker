671602b74108a6c3c1df8fbda9f9b9a8b1c3497c
Jeffrey Allen <jeffreyallen92@gmail.com>
2017-03-06 13:15:54 -0600

    Added textfield. Created viewcontroller delegate. Created method which updates label based on textfield change

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,17 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    // MARK: PROPERTIES
+    // IBOutlet  : tells Xcode that you can connect to the nameTextField
+    //             from the interface builder
+    //  weak     : does not prevent the system from
+    //             deallocating the referenced object
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // Handle the text field’s user input through delegate callbacks.
+        nameTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard
+        textField.resignFirstResponder()
+        return true;
+    }
+    
+    // Called when textfield loses first responder status
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text;
+    }
+    
+    
+    // MARK: ACTIONS
+    // sender : refers to the object that was responsible for triggering the action
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
+    
 
 
 }
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # FoodTracker
+
+## Lesson 1: Building a basic UI
+Familiarized with the contents of an Xcode project, and with many of the tools used to design and run an iOS app. Built a simple user interface
+
+Made sure the layout is robust and extensible from the start ensures that you have a solid foundation to build upon.
+
+## Lesson 2: Connect the UI to code
+
+Used the assistant editor to add outlets (link ) and actions to your source code. You’ve also added code to update the user interface as the user interacts with the controls. The project is still just a relatively simple, single scene, but you will continue to add features and increase its complexity over the remaining lessons.
+
++ Explain the relationship between a scene in a storyboard and the underlying view controller
++ Create outlet and action connections between UI elements in a storyboard and source code
++ Process user input from a text field and display the result in the UI
++ Make a class conform to a protocol
++ Understand the delegation pattern
++ Follow the target-action pattern when designing app architecture
