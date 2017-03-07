594c94454d250d6d6c8e6993870ec16d5efb8bba
Jeffrey Allen <jeffreyallen92@gmail.com>
2017-03-07 15:58:11 -0600

    L5:C2 Designed interface of custom table cell

---
+++ FoodTracker/MealCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MealCollectionViewCell.swift
+//  FoodTracker
+//
+//  Created by Jeffrey Allen on 3/7/17.
+//  Copyright © 2017 Jeffrey Allen. All rights reserved.
+//
+
+import UIKit
+
+class MealCollectionViewCell: UICollectionViewCell {
+    
+}
